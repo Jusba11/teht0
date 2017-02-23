6da9d04106b45c9e0542f742f31ef8952ccd9a48
K8270 <K8270@OHJE-330-WS-24>
2017-02-23 14:24:30 +0200

    asd

--- UI_design/MainPage.xaml.cs
+++ UI_design/MainPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,22 @@ namespace UI_design
         public MainPage()
         {
             this.InitializeComponent();
+
+            //change default startupmode
+            ApplicationView.PreferredLaunchWindowingMode
+                = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            //size 800 x 600
+            ApplicationView.PreferredLaunchViewSize = new Size(800, 600);
+
+            //disable debugging info
+            App.Current.DebugSettings.EnableFrameRateCounter = false;
+            
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("name:" + nameTextbox.Text);
+            Debug.WriteLine("email:" + emailTextbox.Text);
         }
     }
 }
